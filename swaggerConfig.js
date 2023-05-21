b1d40aa1e75bb8b93db7689ec2c7dbb159923525
welinton <wesgdeveloper@gmail.com>
2023-05-21 13:29:27 -0500

    agrego archivo de configuracion swagger

---
+++ swaggerConfig.js
@@ -0,0 +1,35 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+///const fs = require("fs"); //// lo use para generar archibo json de swagger para pages
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de TO-DO LIST",
+      version: "1.0.0",
+      description: "Una API para administrar un To-Do list",
+    },
+    servers: [
+      {
+        url: "http://localhost:3001",
+        description: "Servidor de desarrollo",
+      },
+    ],
+  },
+  apis: ["./index.js"], // Ruta a los archivos de ruta donde están definidas las rutas
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+// // Generar la especificación de Swagger en formato JSON
+// const swaggerJson = JSON.stringify(swaggerSpec, null, 2);//// lo use para generar archibo json de swagger para pages
+
+// // Escribir la especificación en un archivo swagger.json
+// fs.writeFileSync("swagger.json", swaggerJson, "utf8");//// lo use para generar archibo json de swagger para pages
+
+// console.log("Swagger specification generated successfully.");//
+
+module.exports = {
+  swaggerUi,
+  swaggerSpec,
+};
