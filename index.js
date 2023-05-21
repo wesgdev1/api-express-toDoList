ab593ca74a2520a8c557b5eea912a177f229de92
welinton <wesgdeveloper@gmail.com>
2023-05-21 12:51:05 -0500

    se instala librerias de uuii cors y swagger

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-const { swaggerUi, swaggerSpec } = require("./swaggerConfig");
+//const { swaggerUi, swaggerSpec } = require("./swaggerConfig");
 const cors = require("cors");
 const app = express();
 
-let tasks = [];
+let tasks = [
+  {
+    id: "0c3aea71-6d17-4a0e-ac37-8a0175f790b1",
+    name: "elvergalarwerwerwergrraff34343434FGFGFDFDFDFGDFDFFG",
+    email: "xxxxxxxx@hotmail.com34343RTRTRT4",
+    phone: "QUE CALOR2",
+  },
+];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1>ToDo List</h1>");
 });
 
 app.get("/api/tasks", (request, response) => {
-  response.json(notes);
+  response.json(tasks);
 });
 
 const PORT = 3001;
