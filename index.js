6d08bc07138cf796f968500bdfd274fbe68d891f
welinton <wesgdeveloper@gmail.com>
2023-05-21 12:00:40 -0500

    agregamos librerias para uso correcto de la API

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const { swaggerUi, swaggerSpec } = require("./swaggerConfig");
+const cors = require("cors");
 const app = express();
 
 let tasks = [];
