701062062a583e7c6e0412e3d1b627b6583a786f
welinton <wesgdeveloper@gmail.com>
2023-05-21 11:58:53 -0500

    actualizamos a modelo a utilizar para ToDo List

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-let notes = [];
+let tasks = [];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/tasks", (request, response) => {
   response.json(notes);
 });
 
